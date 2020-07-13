e128c57ed1a813be5786a561b5f2a1a67e407651
Peeyush Yadav <peeyushy95@gmail.com>
2020-07-13 22:43:11 +0530

    logger setting

--- src/main/java/com/web/apiController.java
+++ src/main/java/com/web/apiController.java
@@ -1,5 +1,8 @@
 package com.web;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class apiController {
+
+//    Logger logger = LoggerFactory.getLogger(apiController.class);
 
     @GetMapping("/health")
     public ResponseEntity<String> health(){
+
+        log.info("Health");
         return new ResponseEntity<>("Its working", HttpStatus.OK);
     }
 
